a30a6355c69568976f344127fe9a9f344766476e
rogerairgood@gmail.com <rogerairgood@gmail.com>
2020-04-15 16:10:24 -0700

    post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,8 @@
+nstall ArchiveBox to /root/ArchiveBox and add PATH
+git clone https://github.com/pirate/ArchiveBox.git 
+setenv PATH /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin:/root/bin:/root/ArchiveBox/bin
+
+
+
+
+
